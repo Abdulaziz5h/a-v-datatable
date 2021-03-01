0504259cea43d9861940f30f4a0e13d99bc978e8
ash79 <ash79@DESKTOP-CASVFCF>
2021-03-01 17:22:26 +0200

    main stacture

---
+++ vue-table/src/App.vue
@@ -0,0 +1,108 @@
+<template>
+    <div id="app">
+        <vue-colabse-table
+            label="label"
+            align="center"
+            :headers="header"
+            colapsed
+            :childrenHeader="{
+                enable: true,
+                header: [
+                    {
+                        label: 'اسم الوحدة',
+                        value: 'subjectName'
+                    },
+                    {
+                        label: 'الحسم',
+                        value: 'discount'
+                    },
+                    {
+                        label: 'السعر',
+                        value: 'price'
+                    }
+                ]
+            }"
+            :items="items"
+            borderd
+        >
+        </vue-colabse-table>
+    </div>
+</template>
+
+<script>
+import vueColabseTable from "./vue-colabse-table/vue-colabse-table";
+export default {
+    name: "App",
+    components: {
+        vueColabseTable
+    },
+    data() {
+        return {
+            header: [
+                {
+                    label: "اسم المادة",
+                    value: "subjectName"
+                },
+                {
+                    label: "السعر",
+                    value: "price",
+                    type: "number"
+                }
+            ],
+            items: [
+                {
+                    id: 0,
+                    subjectName: "Senior Devs",
+                    price: 0.15296
+                },
+                {
+                    id: 0,
+                    subjectName: "Senior Devs",
+                    price: 0.15296,
+                    children: [
+                        {
+                            id: 1,
+                            subjectName: "John",
+                            discount: 20,
+                            price: 0.03845
+                        },
+                        {
+                            id: 3,
+                            subjectName: "Susan",
+                            discount: 16,
+                            price: 0.08503
+                        }
+                    ]
+                },
+                {
+                    id: 0,
+                    subjectName: "Senior Devs",
+                    price: 0.15296,
+                    children: [
+                        {
+                            id: 1,
+                            subjectName: "John",
+                            discount: 20,
+                            price: 0.03845
+                        },
+                        {
+                            id: 2,
+                            subjectName: "Jane",
+                            discount: 24,
+                            price: 0.02948
+                        },
+                        {
+                            id: 3,
+                            subjectName: "Susan",
+                            discount: 16,
+                            price: 0.08503
+                        }
+                    ]
+                }
+            ]
+        };
+    }
+};
+</script>
+
+<style lang="scss"></style>
---
+++ vue-table/src/vue-colabse-table/vue-colabse-table.vue
@@ -0,0 +1,89 @@
+<template>
+    <tr>
+        <td colspan="100%" style="overflow: hidden">
+            <transition name="slide">
+                <table
+                    v-if="open"
+                    class="vc__table"
+                    :class="[{ borderd: !!borderd }, tableClass]"
+                    border="0"
+                    cellspacing="0"
+                    cellpadding="0"
+                >
+                    <thead class="vc__table__thead" v-if="!!isHeader">
+                        <slot name="header">
+                            <tr
+                                class="vc__table__tr vc__thead__tr"
+                                :class="[theadRowClass]"
+                                :style="[align]"
+                            >
+                                <th
+                                    v-for="(th, index) in headers"
+                                    :key="'sub_th_' + index"
+                                >
+                                    {{ th[label] }}
+                                </th>
+                            </tr>
+                        </slot>
+                    </thead>
+                    <caption v-if="!items">
+                        لا يوجد بيانات
+                    </caption>
+                    <tbody v-else class="vc__table__tbody">
+                        <slot name="body">
+                            <tr
+                                class="vc__table__tr vc__tbody__tr"
+                                :class="[theadRowClass]"
+                                :style="[align]"
+                                v-for="(tr, index) in items"
+                                :key="'sub_tr_' + (mainTableLength + index)"
+                            >
+                                <td
+                                    v-for="(th, i) in headers"
+                                    :key="tr[th.value] + i"
+                                >
+                                    {{ tr[th.value] }}
+                                </td>
+                            </tr>
+                        </slot>
+                    </tbody>
+                    <slot name="footer"></slot>
+                </table>
+            </transition>
+        </td>
+    </tr>
+</template>
+<script>
+export default {
+    props: {
+        open: Boolean,
+        // style
+        align: Object,
+        mainTableLength: Number,
+        isHeader: Boolean,
+        // table header row
+        headers: {
+            type: Array,
+            required: true
+        },
+        label: {
+            type: String,
+            required: true
+        },
+        // table body rows
+        items: {
+            type: Array
+        },
+        // custom classes
+        tableClass: Array,
+        theadRowClass: Array,
+        tbodyRowClass: Array,
+        borderd: Boolean
+    },
+    watch: {
+        open(c) {
+            console.log(c);
+        }
+    }
+};
+</script>
